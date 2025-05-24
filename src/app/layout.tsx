84fabddbc684b80ab2684f4357dba5e37f59b0e0
Nicholas J. Aquino <nicholas@escra.io>
2025-05-24 00:59:16 -0400

    feat: update contract details modal - implement consistent PDF viewer functionality for document actions

--- src/app/layout.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
- 
-  <header className="p-4 border-b border-gray-200 flex items-center justify-end space-x-4">
-    {/* Removed notification bell, search bar, and JD avatar */}
-  </header> 
