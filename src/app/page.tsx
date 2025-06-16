83cc46cf1c35ec7ca141bf5685ef27ee80886187
Nicholas J. Aquino <nicholas@escra.io>
2025-06-16 12:08:32 -0400

    chore: update project structure - escra-frontend as submodule

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function RootTestPage() {
-  return (
-    <div>
-      <h1>Root Page Test</h1>
-      <p>This is a test page to see if the root route is working.</p>
-    </div>
-  );
-}
